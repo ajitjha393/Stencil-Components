6b2c86abf595937fb241667904156479e101cde4
Bishwajit Jha <ajitjha393@gmail.com>
2020-08-17 09:49:21 +0530

    Added button for closing side Drawer using mutable Props open

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -12,14 +12,20 @@ export class SideDrawer {
 	title: string
 
 	@Prop({
-		reflect: true
-	}) open: boolean = false
+		reflect: true,
+	})
+	open: boolean
+
+	onCloseDrawer = () => {
+		this.open = false
+	}
 
 	render() {
 		return (
 			<aside>
 				<header>
 					<h1>{this.title}</h1>
+					<button onClick={this.onCloseDrawer}>X</button>
 				</header>
 				<main>
 					<slot />
