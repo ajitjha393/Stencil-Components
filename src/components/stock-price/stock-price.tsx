ad6e163dad9a74a7f02c10c6704c9ccb4a75d96c
Bishwajit Jha <ajitjha393@gmail.com>
2020-08-18 11:29:11 +0530

    Added method for handling/fetching stock price on submit

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -6,9 +6,14 @@ import { Component, h } from '@stencil/core'
 	shadow: true,
 })
 export class StockPrice {
+	onFetchStockPrice(event: Event) {
+		event.preventDefault()
+		console.log('Fetching Stock Price!')
+	}
+
 	render() {
 		return [
-			<form>
+			<form onSubmit={this.onFetchStockPrice}>
 				<input type="text" id="stock-symbol" />
 				<button type="submit">Fetch</button>
 			</form>,
